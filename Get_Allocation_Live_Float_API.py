38991179664bacc5ab0e79bc5fc7f999943cd7f3
aminh-airfoil <aminh@airfoil.studio>
2025-06-17 19:43:35 +0800

    Update Get_Allocation_Live_Float_API.py

--- Get_Allocation_Live_Float_API.py
+++ Get_Allocation_Live_Float_API.py
@@ -3,7 +3,7 @@ import os
 
 # Load secrets from environment
 float_api_token = os.getenv("FLOAT_API_TOKEN")
-n8n_allocation_webhook_url = os.getenv("N8N_ALLOCATION_LIVE_WEBHOOK_URL")
+n8n_allocation_webhook_url = os.getenv("N8N_ALLOCATION_WEBHOOK_URL")
 float_url = "https://api.float.com/v3/tasks?page=1&per_page=50"
 
 # Auth headers
@@ -22,7 +22,7 @@ if response.status_code != 200:
 data = response.json()
 
 # Send to n8n webhook
-webhook_response = requests.post(n8n_allocation_live_webhook_url, json=data)
+webhook_response = requests.post(n8n_allocation_webhook_url, json=data)
 
 # Confirm POST status
 if webhook_response.status_code == 200:
